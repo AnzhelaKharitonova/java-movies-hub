91ec50125bcb93b1211c1f70cad0dd1984cb41b2
AnzhelaKharitonova <haritonova.azazel88@yandex.ru>
2026-05-21 22:02:03 +0300

    Раскрыть импорты

--- src/main/ru/practicum/moviehub/store/MoviesStore.java
+++ src/main/ru/practicum/moviehub/store/MoviesStore.java
@@ -3,7 +3,10 @@ package ru.practicum.moviehub.store;
 import ru.practicum.moviehub.model.InputException;
 import ru.practicum.moviehub.model.Movie;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class MoviesStore {
     private final Map<Integer, Movie> movies = new HashMap<>();
